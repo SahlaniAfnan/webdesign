36912f0a9f008a7f7ea889be05c0926189aee310
SahlaniAfnan <sahlani.afnan@gmail.com>
2018-05-17 22:13:18 +0700

    Mengupdate/Mengintegrasikan Header.php (template) dengan css

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,41 +1,24 @@
+
 <!DOCTYPE html>
-  <html>
-    <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-      <title><?php echo $pageTitle; ?></title>
-      <style>
-        body {
-          width: 100%;
-          height: 100%;
-          padding: 0px;
-          margin: 0px;
-        }
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+    <title><?php echo $pageTitle; ?> | ACCSEGERAN Admin</title>
 
-        header {
-          width: 100%;
-          background-color: red;
-        }
-
-        footer {
-          width: 100%;
-          background-color: orange;
-        }
-
-        #content {
-          width: 69%;
-          background-color: blue;
-          display: inline-block;
-        }
-
-        #sidebar {
-          width: 30%;
-          background-color: green;
-          display: inline-block;
-        }
-      </style>
-    </head>
-    <body>
-      <header>
-        <h1>Belajar Template Codeigniter</h1>
-      </header>
-      <div class="container">
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link href="<?php echo base_url('asset/css/materialize.css'); ?>" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="<?php echo base_url('asset/css/sahlan.css'); ?>" type="text/css" rel="stylesheet" media="screen,projection"/>
+  </head>
+  <body>
+    <header>
+      <nav class="light-blue lighten-1 navbar-fixed" role="navigation">
+        <div class="nav-wrapper container">
+          <a id="logo-container" href="<?php echo base_url('#'); ?>" class="brand-logo center">
+            <img src="<?php echo base_url('asset/images/logo.png'); ?>" alt="accsegeran Logo">
+          </a>
+          <a href="#" data-activates="sidenav" class="button-collapse"><i class="material-icons">menu</i></a>
+        </div>
+      </nav>
+    </header>
