e5723396decea029794932735a3cb6e1eb30c61c
SahlaniAfnan <sahlani.afnan@gmail.com>
2018-05-18 01:16:48 +0700

    Update Dashboard.php
    untuk meng-extends MY_Controller dan tambahkan function __construct(). Function construct ini adalah method yang pertama kali dijalankan saat sebuah kelas dipanggil atau digunakan. Bisa kita lihat di dalam function construct ini kita juga memanggil $this->cekLogin(), dimana $this berarti class Dashboard itu sendiri dan karena kita sudah meng-extends core controller maka si class Dashboard ini bisa menggunakan semua function yang berada di dalam MY_Controller.

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,16 +1,23 @@
-<?php defined("BASEPATH") OR exit("No direct script access allowed");
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-  class Dashboard extends CI_Controller {
-
-    public function index()
-    {
-      // Data ini akan ditampilkan di header.php pada tag <title>
-      $data['pageTitle'] = 'Dashboard';
-
-      // Data ini akan ditampilkan di content.php
-      $data['pageContent'] = $this->load->view('dashboard/main.php', NULL, TRUE);
-
-      // Memanggil view layout.php
-      $this->load->view("template/layout", $data);
-    }
+class Dashboard extends MY_Controller
+{
+  public function __construct()
+  {
+    parent::__construct();
+    $this->cekLogin();
   }
+
+  public function index()
+  {
+    $data['pageTitle'] = 'Dashboard';
+    $data['pageContent'] = '<h1>Ini fungsi index dari controller dashboard</h1><h1>Ini fungsi index dari controller dashboard</h1><h1>Ini fungsi index dari controller dashboard</h1>';
+
+    $this->load->view('template/layout', $data);
+  }
+
+  public function routing_ke()
+  {
+    echo 'Hahaha';
+  }
+}
