8ad0f3b2f7ce7954c626cc8b4ecdf3e09d2a52df
SahlaniAfnan <sahlani.afnan@gmail.com>
2018-05-18 01:45:55 +0700

    Mengupdate Template Sidebar
    mengupdate sidebar.php yang berada di dalam folder application/views/layout/sidebar.php. Disini kita hanya akan menambahkan sebuah menu logout yang akan mengarahkan kita ke controller auth/logout dan menghapus semua session user jika di klik.

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -1,19 +1,20 @@
-
-
 <aside>
   <ul id='sidenav' class='side-nav fixed'>
     <li>
       <div class='userView'>
         <div class='background'>
-          <img src='<?php echo base_url('assets/images/nav.jpg'); ?>'>
+          <img src='<?php echo base_url('asset/image/nav6.jpg'); ?>'>
         </div>
-        <a href='#!user'><img class='circle' src='<?php echo base_url('assets/images/noavatar.png'); ?>'></a>
-        <a href='#!name'><span class='white-text name'>Sahlani Afnan</span></a>
-        <a href='#!email'><span class='white-text email'>sahlani.afnan@gmail.com</span></a>
+        <a href='#!user'><img class='circle' src='<?php echo base_url('asset/image/noavatar.png'); ?>'></a>
+        <a href='#!name'><span class='white-text name'>SAHLANI AFNAN</span></a>
+        <a href="#!email"><span class="white-text email">sahlani.afnan@gmail.com</span></a>
       </div>
     </li>
     <li>
-      <a class='waves-effect' href='<?php echo base_url('dashboard'); ?>'><i class='material-icons'>home</i>Dashboard</a>
+      <a class='waves-effect'  href='<?php echo base_url('dashboard'); ?>'><i class='material-icons'>home</i>Dashboard</a>
+    </li>
+    <li>
+      <a class='waves-effect'  href="<?php echo base_url('auth/logout'); ?>"><i class='material-icons'>exit_to_app</i>Logout</a>
     </li>
   </ul>
 </aside>
