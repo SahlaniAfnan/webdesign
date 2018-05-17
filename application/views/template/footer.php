502f803a2ff6f3ac3a5224570f6d4c2d0f16c1cd
SahlaniAfnan <sahlani.afnan@gmail.com>
2018-05-17 22:14:18 +0700

    Mengupdate/Mengintegrasikan footer.php (template) dengan css

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,8 +1,14 @@
-</div>
-<footer>
-	<p>Copyright &copy; ACCSEGERAN</p>
-	<p>Contact information: <a href="mailto:sahlani.afnan@gmail.com">
-	sahlani.afnan@gmail.com</a>.</p>
+<footer class='page-footer white'>
+	<div class='footer-copyright amber darken-1'>
+		<div class='container center'>
+			Copyright &copy; <a class='white-text text-lighten-3' href='https://www.accsegeran.com'>ACCSEGERAN</a>
+		</div>
+	</div>
 </footer>
+
+<!-- Scripts -->
+<script src='https://code.jquery.com/jquery-2.1.1.min.js'></script>
+<script src='<?php echo base_url('asset/js/materialize.js'); ?>'></script>
+<script src='<?php echo base_url('assets/js/kcdev.js'); ?>'></script>
 </body>
 </html>
